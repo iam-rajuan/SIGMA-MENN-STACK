f534e82c9aae99448e414b977dec1db4100980e9
Md. Rajuan Hossen <rajuan.official@gmail.com>
2025-02-26 01:31:23 +0600

    ReactJSX

---
+++ ReactJSX/my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+import Navbar from './components/navbar';
+
+function App() {
+  const [value, setValue] = useState(0)
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="value">{value}</div>
+      <button onClick={()=>{setValue(value +10)}}>click me</button>
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ ReactJSX/my-app/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
